640b5879f1f0f035c7f6d587c95cdec071a930e2
aishydixit <53096942+aishydixit@users.noreply.github.com>
2019-07-24 23:36:56 +0530

    Add files via upload
    prime number

---
+++ prime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+   int a, b, count, i, j;
+ 
+   printf("Enter two numbers:");
+   
+   scanf("%d %d", &a, &b);
+ 
+   
+   printf("Prime numbers from %d and %d are:\n",a, b);
+   for(i=a+1; i<b; ++i)
+   {
+      count=0;
+      for(j=2; j<=i/2; ++j)
+      {
+         if(i%j==0)
+         {
+            count=1;
+            break;
+         }
+      }
+      if(count==0)
+         printf("%d\n",i);
+  }
+  return 0;
+}
